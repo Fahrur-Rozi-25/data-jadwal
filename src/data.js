894214998301e6c437f04a147dd2f04616cb4d41
Fahrur <akungamesaya1234566@gmail.com>
2023-05-18 18:19:41 +0700

    first commit

---
+++ src/data.js
@@ -0,0 +1,15 @@
+
+
+
+const data = [
+    {pelajaran: 'B arab'},
+    {pelajaran: 'B indo'},
+    {pelajaran: 'B arab'},
+    {pelajaran: 'B inggris'},
+    {pelajaran: 'B inggris'},
+    {pelajaran: 'B inggris'},
+]
+
+
+
+
---
+++ src/index.js
@@ -0,0 +1,21 @@
+
+const datax = ['dd']
+
+ 
+function index () {
+
+  for (let i = 0; i < datax.length; i++) {
+    console.log(datax[i]);
+    const tdx = document.createElement('td')
+
+    return tdx
+}
+}
+
+
+
+
+if(day == 'Kamis') {
+  console.log('sekarang kamis')
+} 
+
