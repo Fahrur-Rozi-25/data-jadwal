2af47d5f30eb8aa49d2b0eeb6b914f006e4ed059
Fahrur <akungamesaya1234566@gmail.com>
2023-06-01 18:34:11 +0700

    p

--- src/data.js
+++ src/data.js
@@ -1,15 +1,25 @@
 
-
-
-const data = [
-    {pelajaran: 'B arab'},
-    {pelajaran: 'B indo'},
-    {pelajaran: 'B arab'},
-    {pelajaran: 'B inggris'},
-    {pelajaran: 'B inggris'},
-    {pelajaran: 'B inggris'},
-]
+const url = 'http://127.0.0.1/API/public/api/me'
+const methods = 'GET'
+const token = 'Bearer 5|McCz05ChA0gMoh91jdNdGczBwm8HuWEQkZdaIZoI'
 
 
 
-
+fetch(url, {
+  headers: {
+    'Authorization': token
+  } ,method: methods
+})
+  .then(function(response) {
+    if (response.ok) {
+      return response.json(); // Mengembalikan respons sebagai objek JSON
+    }
+    throw new Error('Error: ' + response.status);
+  })
+  .then(function(data) {
+    // Lakukan sesuatu dengan data response
+    console.log(data);
+  })
+  .catch(function(error) {
+    console.log(error);
+  });
